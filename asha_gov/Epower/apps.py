8e124ff9505cac1fd01504e732a40c00f7c6da48
shani <kumarpc2795@gmail.com>
2026-09-03 14:00:00 +0530

    Delete asha_gov directory

--- asha_gov/Epower/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EpowerConfig(AppConfig):
-    name = 'Epower'
--- asha_gov/Epower/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('epower/<int:pk>/', views.epower_detail, name='epower_detail'),
-]
